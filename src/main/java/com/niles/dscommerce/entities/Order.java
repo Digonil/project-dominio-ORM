efad2f3a084fce88040901c49e165140e793002f
Digonil <diego.niles@hotmail.com>
2023-06-04 12:40:13 -0300

    Criado o OrderItem e o OrderItemPK

--- src/main/java/com/niles/dscommerce/entities/Order.java
+++ src/main/java/com/niles/dscommerce/entities/Order.java
@@ -4,7 +4,10 @@ import jakarta.persistence.*;
 
 import java.io.Serializable;
 import java.time.Instant;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Objects;
+import java.util.Set;
 
 @Entity
 @Table(name = "tb_order")
@@ -25,6 +28,9 @@ public class Order implements Serializable {
     @ManyToOne
     @JoinColumn(name = "client_id")
     private User client;
+
+    @OneToMany(mappedBy = "id.order")
+    private Set<OrderItem> items = new HashSet<>();
 
     public Order() {
     }
@@ -75,6 +81,14 @@ public class Order implements Serializable {
 
     public void setClient(User client) {
         this.client = client;
+    }
+
+    public Set<OrderItem> getItems() {
+        return items;
+    }
+
+    public List<Product> getProduct() {
+        return items.stream().map(x -> x.getProduct()).toList();
     }
 
     @Override
