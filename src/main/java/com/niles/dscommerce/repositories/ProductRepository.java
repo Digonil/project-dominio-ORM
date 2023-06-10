d2b90852945e3581c33b5053032eec3583e9940e
Digonil <diego.niles@hotmail.com>
2023-06-10 12:36:15 -0300

    Realizado  a busca de produto pelo id

---
+++ src/main/java/com/niles/dscommerce/repositories/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.niles.dscommerce.repositories;
+
+import com.niles.dscommerce.entities.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
