4a587a168a53ba9aab2282882ba1227f96d33e4f
Digonil <diego.niles@hotmail.com>
2023-06-11 19:31:33 -0300

    Adicionado método de deletar produto

--- src/main/java/com/niles/dscommerce/controllers/ProductController.java
+++ src/main/java/com/niles/dscommerce/controllers/ProductController.java
@@ -40,8 +40,14 @@ public class ProductController {
 
     @PutMapping(value = "/{id}")
     public ResponseEntity<ProductDTO> update(@PathVariable Long id, @RequestBody ProductDTO dto) {
-        dto = service.update(id, dto);
-        return ResponseEntity.ok(dto);
+        var updateDto = service.update(id, dto);
+        return ResponseEntity.ok(updateDto);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity delete(@PathVariable Long id) {
+        service.delete(id);
+        return ResponseEntity.noContent().build();
     }
 
 
