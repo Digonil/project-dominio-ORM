8e127eb22ebe9db5eb686b290ce565008ac30ce5
Digonil <diego.niles@hotmail.com>
2023-06-19 22:05:51 -0300

    Tratamento de excessões

--- src/main/java/com/niles/dscommerce/controllers/handlers/ControllerExceptionHandler.java
+++ src/main/java/com/niles/dscommerce/controllers/handlers/ControllerExceptionHandler.java
@@ -1,11 +1,11 @@
 package com.niles.dscommerce.controllers.handlers;
 
 import com.niles.dscommerce.dto.CustomError;
+import com.niles.dscommerce.services.exceptions.DatabaseException;
 import com.niles.dscommerce.services.exceptions.ResourceNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.jdbc.support.CustomSQLExceptionTranslatorRegistrar;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -16,6 +16,13 @@ public class ControllerExceptionHandler {
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<CustomError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request) {
         HttpStatus status = HttpStatus.NOT_FOUND;
+        CustomError err = new CustomError(Instant.now(), status.value(), e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
+    }
+
+    @ExceptionHandler(DatabaseException.class)
+    public ResponseEntity<CustomError> database(DatabaseException e, HttpServletRequest request) {
+        HttpStatus status = HttpStatus.BAD_REQUEST;
         CustomError err = new CustomError(Instant.now(), status.value(), e.getMessage(), request.getRequestURI());
         return ResponseEntity.status(status).body(err);
     }
